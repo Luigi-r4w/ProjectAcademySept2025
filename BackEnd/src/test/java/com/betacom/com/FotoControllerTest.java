ca8440cc3b563da7695b445e9ee0228b668b90d4
eliaCigana <ciganaelia5@gmail.com>
2025-09-15 10:11:23 +0200

    controller test

--- BackEnd/src/test/java/com/betacom/com/FotoControllerTest.java
+++ BackEnd/src/test/java/com/betacom/com/FotoControllerTest.java
@@ -1,7 +1,6 @@
 package com.betacom.com;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
@@ -11,21 +10,21 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.betacom.com.controller.FotoController;
 import com.betacom.com.dto.FotoDTO;
 import com.betacom.com.exception.AcademyException;
-import com.betacom.com.repositories.IFotoRepository;
 import com.betacom.com.request.FotoReq;
-import com.betacom.com.services.interfaces.IFotoServices;
+import com.betacom.com.response.ResponseBase;
+import com.betacom.com.response.ResponseList;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class FotoServicesTest {
+public class FotoControllerTest {
 	
-	@Autowired IFotoRepository fotoR;
-	@Autowired IFotoServices fotoS;
+	@Autowired FotoController fotoC;
 	
 	@Test
 	@Order(1)
@@ -47,11 +46,11 @@ public class FotoServicesTest {
 		req.setWidthResolution(1920);
 		req.setHeightResolution(1080);
 		
-		fotoS.createFoto(req);
+		fotoC.create(req);
 		
-		List<FotoDTO> listFoto = fotoS.listAll();
+		ResponseList<FotoDTO> listFoto = fotoC.list();
 		
-		FotoDTO f = listFoto.stream()
+		FotoDTO f = listFoto.getList().stream()
 				.filter(e -> e.getId() == 1)
 				.findFirst()		
 				.orElseThrow(() -> new AssertionError("Foto non trovata"));
@@ -61,6 +60,32 @@ public class FotoServicesTest {
 	
 	@Test
 	@Order(2)
+	void errorCreateFotoTest() throws AcademyException {
+		log.debug("Test Method: errorCreateFotoTest()");
+		
+		FotoReq req = new FotoReq();
+		
+		req.setPrezzo(150.75); 
+		req.setDescrizione("Un'opera d'arte digitale che raffigura un paesaggio astratto."); 
+		req.setTitolo("Paesaggio Astratto al Tramonto"); 
+		req.setDataCreazione(LocalDate.of(2025, 9, 12)); 
+		req.setDimensione("1920x1080"); 
+		req.setAutore("Nome Autore"); 
+		req.setImmagine("abstract_sunset.jpg"); 
+		req.setIsAI(true); 
+		// req.setCategoria("Arte Digitale"); -> categoria null
+		req.setDevice("Iphone 15 pro MAX");
+		req.setWidthResolution(1920);
+		req.setHeightResolution(1080);
+		
+		ResponseBase rB = fotoC.create(req);
+		
+		
+		Assertions.assertThat(rB.getRc()).isEqualTo(false);
+	}
+	
+	@Test
+	@Order(3)
 	void updateFotoTest() throws AcademyException{
 		log.debug("Test Method: updateFotoTest()");
 		
@@ -68,11 +93,11 @@ public class FotoServicesTest {
 		req.setId(1);
 		req.setAutore("Manzoni");
 		
-		fotoS.updateFoto(req);
+		fotoC.update(req);
 		
-		List<FotoDTO> listFoto = fotoS.listAll();
+		ResponseList<FotoDTO> listFoto = fotoC.list();
 		
-		FotoDTO f = listFoto.stream()
+		FotoDTO f = listFoto.getList().stream()
 				.filter(e -> e.getId() == 1)
 				.findFirst()		
 				.orElseThrow(() -> new AssertionError("Foto non trovata"));
@@ -81,18 +106,50 @@ public class FotoServicesTest {
 	}
 	
 	@Test
-	@Order(3)
+	@Order(4)
+	void errorUpdateFotoTest() throws AcademyException{
+		log.debug("Test Method: errorUpdateFotoTest()");
+		
+		FotoReq req = new FotoReq();
+		req.setId(2);
+		req.setAutore("Manzoni");
+		
+		ResponseBase rB = fotoC.update(req);
+		
+		
+		Assertions.assertThat(rB.getRc()).isEqualTo(false);
+	}
+	
+	@Test
+	@Order(5)
 	void deleteFotoTest() throws AcademyException{
 		log.debug("Test Method: deleteFotoTest()");
 		
 		FotoReq req = new FotoReq();
 		req.setId(1);
 		
-		fotoS.deleteFoto(req);
+		fotoC.delete(req);
 		
-		List<FotoDTO> listFoto = fotoS.listAll();
+		ResponseList<FotoDTO> listFoto = fotoC.list();
 		
-		Assertions.assertThat(listFoto.size()).isEqualTo(0);
+		Assertions.assertThat(listFoto.getList().size()).isEqualTo(0);
+		
+	}
+	
+	@Test
+	@Order(5)
+	void errorDeleteFotoTest() throws AcademyException{
+		log.debug("Test Method: errorDeleteFotoTest()");
+		
+		FotoReq req = new FotoReq();
+		req.setId(1);
+		
+
+		ResponseBase rB = fotoC.delete(req);
+		
+		
+		Assertions.assertThat(rB.getRc()).isEqualTo(false);
+	
 	}
 
 }
