1307587739da0ece42daaa57d384ce7bcdc6ff99
Luigi Casazza <casazza.luigi01@gmail.com>
2025-09-23 18:50:10 +0200

    localStorage auth

--- FrontEnd/src/app/services/auth/auth.ts
+++ FrontEnd/src/app/services/auth/auth.ts
@@ -1,43 +1,81 @@
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class Auth {
-
-  isLogged = false;
-  id!: number;
-  isAdmin = false;
   
-  constructor() {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
 
-  isAutentificated() {
-    return this.isLogged;
+    if (isPlatformBrowser(this.platformId)) {
+      console.log('AUTH...')
+      const isLoggedInValue = localStorage.getItem("isLoggedIn");
+      const isAdminValue = localStorage.getItem("isAdmin");
+      const idValue = localStorage.getItem('idUsr');
+  
+        if (isLoggedInValue !== null && isAdminValue !== null && idValue !== null) {
+          console.log("Token exists");
+        } else {
+          localStorage.setItem("isLoggedIn", "0");
+          localStorage.setItem("isAdmin", "0");
+          localStorage.setItem("idUsr", "0");
+        }
+      }
   }
 
-  setId(iD : number){
-    this.id=iD;
+  isAutentificated() {
+    if (isPlatformBrowser(this.platformId)) {
+      if (localStorage.getItem("isLoggedIn") == '1'){
+        console.log('è autenticato')
+        return true;
+      }
+    }
+    return false;
+  }
+
+  setId(id : number){
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('idUsr', id.toString());
+    }
   }
 
   getId() {
-    return this.id;
+    if (isPlatformBrowser(this.platformId)){
+      console.log('id'+localStorage.getItem("idUsr"))
+      return Number(localStorage.getItem("idUsr"));
+    }
+    return 0;
   }
 
   setAuthentificated() {
-    this.isLogged = true;
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem("isLoggedIn", "1");
+    }
   }
 
   setIsAdmin(){
-    this.isAdmin = true;
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem("isAdmin", "1");
+    }
   }
 
   getIsAdmin(){
-    return this.isAdmin;
+    if (isPlatformBrowser(this.platformId)) {
+      if (localStorage.getItem("isAdmin") == '1'){
+        console.log('è admin')
+        return true;
+      }
+    }
+    return false;
   }
 
   resetAll() {
-    this.isLogged = false;
-    this.id=0;
-    this.isAdmin = false;
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem("isLoggedIn", "0")
+      localStorage.setItem("isAdmin", "0")
+      localStorage.setItem("idUsr", "0");
+    }
   }
 }
